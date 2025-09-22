79b8a40c66e1eab6b40f91e4abc6a56642cf71ed
Nico Barberis <nico09barberis@gmail.com>
2025-09-22 16:42:03 -0300

    ajuste en vite.config.js

--- 500-millas-web/vite.config.js
+++ 500-millas-web/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Asegura que Vite compile en la carpeta que Vercel espera
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // opcional, si usás alias en tus imports
+    },
+  },
+  base: '/', // importante para rutas en producción
+});
