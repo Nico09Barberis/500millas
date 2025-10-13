c2c122e7659750a175ea9301661323fb51b2cc74
Nico Barberis <nico09barberis@gmail.com>
2025-10-13 17:33:29 -0300

    nuevos ajustes visuales en algunas secciones del home

--- 500-millas-web/src/sections/home/AboutSection.jsx
+++ 500-millas-web/src/sections/home/AboutSection.jsx
@@ -1,43 +1,30 @@
-import logo from "../../assets/images/logo-blanco.png";
-import fondo from "../../assets/images/about.jpeg"; // tu imagen de fondo
+import logo from "../../assets/images/logo.png";
 
 const AboutSection = () => {
   return (
-    <section className="py-16 px-6">
-      <div
-        className="relative max-w-5xl mx-auto shadow-sm rounded-3xl overflow-hidden"
-      >
-        {/* Imagen de fondo */}
-        <div
-          className="absolute inset-0 bg-cover bg-center"
-          style={{ backgroundImage: `url(${fondo})` }}
-        ></div>
-
-        {/* Overlay */}
-        <div className="absolute inset-0 bg-black bg-opacity-30"></div>
-
+    <section className="my-12">
+      <div className="relative overflow-hidden bg-white">
         {/* Contenido */}
-        <div className="relative text-center w-full max-w-4xl mx-auto px-4 py-4">
+        <div className="relative text-center w-full max-w-4xl mx-auto px-4 py-12">
           {/* Título */}
-          <h2 className="font-tangerine-regular text-5xl md:text-6xl font-bold text-white mb-4">
+          <h2 className="font-tangerine-regular text-5xl md:text-6xl font-bold text-black mb-4">
             Quiénes Somos
           </h2>
 
           {/* Texto */}
-          <p className="font-cormorant-garamond text-xl md:text-2xl font-semibold italic text-white leading-relaxed mb-0 md:mb-8">
-            Somos una empresa dedicada a brindar productos de calidad y un servicio
-            personalizado. Creemos en la atención cercana, la confianza y el compromiso
-            con cada uno de nuestros clientes. Nuestra misión es ayudarte a encontrar lo
-            que necesitás, acompañándote en cada paso de tu compra.
+          <p className="font-cormorant-garamond text-xl md:text-2xl font-semibold italic text-black leading-relaxed mb-0 md:mb-8">
+            Somos una empresa dedicada a brindar productos totalmente
+            artesanales de la más alta calidad, junto con un servicio
+            personalizado. Creemos en la atención cercana, la confianza y el
+            compromiso con cada uno de nuestros clientes. Nuestra misión es
+            ayudarte a encontrar lo que necesitás, acompañándote en cada paso de
+            tu compra y asegurando que cada producto refleje el cuidado y la
+            dedicación que ponemos en nuestro trabajo.
           </p>
 
           {/* Imagen */}
           <div className="hidden md:flex justify-center">
-            <img
-              src={logo}
-              alt="Logo de la empresa"
-              className="w-24 h-24"
-            />
+            <img src={logo} alt="Logo de la empresa" className="w-24 h-auto" />
           </div>
         </div>
       </div>
--- 500-millas-web/src/sections/home/DataSection.jsx
+++ 500-millas-web/src/sections/home/DataSection.jsx
@@ -4,36 +4,36 @@ import { CiMoneyCheck1 } from "react-icons/ci";
 
 const DataSection = () => {
   return (
-    <section className="py-6 px-6 my-12 bg-gray-100">
-      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+    <section className="py-6 px-6 my-12 max-w-6xl mx-auto bg-gray-100">
+      <div className="flex flex-col lg:flex-row gap-8">
         
         {/* Columna 1 */}
-        <article className="flex flex-col md:flex-row items-center gap-4 p-6">
+        <article className="flex flex-col lg:flex-row items-center gap-4 p-6 flex-1">
           <TbTruckDelivery
             className="text-gray-800 text-4xl shrink-0"
             title="Envíos"
             aria-label="Envíos"
           />
-          <div className="text-center md:text-left">
+          <div className="text-center lg:text-left">
             <h3 className="text-base font-bold uppercase text-gray-800">
               Realizamos envíos a
             </h3>
             <p className="text-base uppercase leading-tight text-gray-800">
-              todas las provincias
+              todo el país
             </p>
           </div>
         </article>
 
         {/* Columna 2 */}
-        <article className="flex flex-col md:flex-row items-center gap-4 p-6">
+        <article className="flex flex-col lg:flex-row items-center gap-4 p-6 flex-1">
           <CiMoneyCheck1
             className="text-gray-800 text-4xl shrink-0"
             title="Métodos de pago"
             aria-label="Métodos de pago"
           />
-          <div className="text-center md:text-left">
+          <div className="text-center lg:text-left">
             <h3 className="text-base font-bold uppercase text-gray-800">
-              Podes pagar con
+              Podés pagar con
             </h3>
             <p className="text-base uppercase leading-tight text-gray-800">
               transferencia, Mercado Pago o efectivo
@@ -42,19 +42,19 @@ const DataSection = () => {
         </article>
 
         {/* Columna 3 */}
-        <article className="flex flex-col md:flex-row items-center gap-4 p-6">
+        <article className="flex flex-col lg:flex-row items-center gap-4 p-6 flex-1">
           <FaWhatsapp
             className="text-gray-800 text-4xl shrink-0"
             title="Atención por WhatsApp"
             aria-label="Atención por WhatsApp"
           />
-          <div className="flex flex-col md:flex-row md:items-center gap-2 text-center md:text-left">
+          <div className="flex flex-col lg:flex-row lg:items-center gap-2 text-center lg:text-left">
             <div>
               <h3 className="text-base font-bold uppercase text-gray-800">
                 Te ayudamos
               </h3>
               <p className="text-base uppercase leading-tight text-gray-800">
-                con tus compras
+                con tu compra
               </p>
             </div>
             <a
