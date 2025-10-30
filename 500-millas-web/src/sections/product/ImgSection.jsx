a8940f6d01a8d4215088a953febc56bad103215f
Nico Barberis <nico09barberis@gmail.com>
2025-10-30 17:38:27 -0300

    minimos ajustes de responsive en productSection

--- 500-millas-web/src/sections/product/ImgSection.jsx
+++ 500-millas-web/src/sections/product/ImgSection.jsx
@@ -3,7 +3,7 @@ import bgImage from "../../assets/images/bg-products.jpg";
 
 const HeroSection = () => {
   return (
-    <div className="relative w-full h-[40vh]">
+    <div className="relative w-full h-[40vh] overflow-hidden">
       {/* Imagen de fondo */}
       <img
         src={bgImage}
@@ -15,11 +15,11 @@ const HeroSection = () => {
       <div className="absolute inset-0 bg-black/40"></div>
 
       {/* Contenido centrado */}
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-center font-ancois-one uppercase text-white px-4">
-        <p className="text-lg md:text-2xl max-w-2xl">
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center font-ancois-one tracking-widest uppercase text-white px-4">
+        <p className="text-xl md:text-3xl max-w-2xl">
           conoce
         </p>
-        <h1 className="text-3xl md:text-5xl font-bold mb-2">
+        <h1 className="text-4xl md:text-6xl font-bold mb-2">
           nuestros productos 
         </h1>
       </div>
